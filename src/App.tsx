ed4466d97673ac9c123395e2f7582b58b12ab108
vumanhhung <vumanhhung2506@gmail.com>
2024-05-15 02:52:04 +0700

    bt ss29

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import B1 from './components/B1'
+import B2 from './components/B2'
+import B3 from './components/b3'
+import B4 from './components/b4'
+import B5 from './components/b5'
+export default function App() {
+  return (
+    <><B1></B1><B2></B2><B3></B3><B4></B4><B5></B5></>
+  )
+}
